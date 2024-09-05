d08b3155f7136e6fa848df8b08d72af6ed55e0e8
gallaugher <john.gallaugher@bc.edu>
2024-09-05 13:08:15 -0400

    Initial Commit

---
+++ ToDoList/ContentView.swift
@@ -0,0 +1,21 @@
+// File: ContentView.swift Project: ToDoList
+// Created by: Prof. John Gallaugher on 9/5/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,14 @@
+// File: ToDoListApp.swift Project: ToDoList
+// Created by: Prof. John Gallaugher on 9/5/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
