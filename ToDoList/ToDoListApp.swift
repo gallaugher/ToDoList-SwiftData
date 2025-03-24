4d83238c3aeb603782a2ceb308b9ee903acd0afe
gallaugher <john.gallaugher@bc.edu>
2025-03-24 19:39:00 -0400

    App is done. Here's complete working code!

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -3,12 +3,19 @@
 // YouTube.com/profgallaugher  -  threads.net/john.gallaugher
 
 import SwiftUI
+import SwiftData
 
 @main
 struct ToDoListApp: App {
     var body: some Scene {
         WindowGroup {
             ToDoListView()
+                .modelContainer(for: ToDo.self)
         }
+    }
+    
+    // Will allow us to find where our simulator datea is saved:
+    init() {
+        print(URL.applicationSupportDirectory.path(percentEncoded: false))
     }
 }
---
+++ ToDoList/Views/ToDoListView.swift
@@ -0,0 +1,117 @@
+// File: ToDoListView.swift Project: ToDoList
+// Created by: Prof. John Gallaugher on 9/5/24
+// YouTube.com/profgallaugher  -  threads.net/john.gallaugher
+
+import SwiftUI
+import SwiftData
+
+enum SortOption: String, CaseIterable {
+    case asEntered = "As Entered"
+    case alphabetical = "A-Z"
+    case chronological = "Date"
+    case completed = "Not Done"
+}
+
+struct SortedToDoList: View {
+    @Query var toDos: [ToDo]
+    @Environment(\.modelContext) var modelContext
+    let sortSelection: SortOption
+    
+    init(sortSelection: SortOption) {
+        self.sortSelection = sortSelection
+        switch self.sortSelection {
+        case .asEntered:
+            _toDos = Query()
+        case .alphabetical:
+            _toDos = Query(sort: \.item)
+        case .chronological:
+            _toDos = Query(sort: \.dueDate)
+        case .completed:
+            _toDos = Query(filter: #Predicate {$0.isCompleted == false})
+        }
+    }
+    
+    var body: some View {
+        List {
+            ForEach(toDos) { toDo in
+                VStack (alignment: .leading) {
+                    HStack {
+                        Image(systemName: toDo.isCompleted ? "checkmark.rectangle" : "rectangle")
+                            .onTapGesture {
+                                toDo.isCompleted.toggle()
+                                guard let _ = try? modelContext.save() else {
+                                    print("😡 ERROR: Save after .toggle in ToDoListView did not work.")
+                                    return
+                                }
+                            }
+                        
+                        NavigationLink {
+                            DetailView(toDo: toDo)
+                        } label: {
+                            Text(toDo.item)
+                        }
+                    }
+                    .font(.title2)
+                    
+                    HStack {
+                        Text(toDo.dueDate.formatted(date: .abbreviated, time: .shortened))
+                            .foregroundStyle(.secondary)
+                        
+                        if toDo.reminderIsOn {
+                            Image(systemName: "calendar.badge.clock")
+                                .symbolRenderingMode(.multicolor)
+                        }
+                    }
+                }
+            }
+            .onDelete { indexSet in
+                indexSet.forEach({modelContext.delete(toDos[$0])})
+                guard let _ = try? modelContext.save() else {
+                    print("😡 ERROR: Save after .delete in ToDoListView did not work.")
+                    return
+                }
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ToDoListView: View {
+    @State private var sheetIsPresented = false
+    @State private var sortSelection: SortOption = .asEntered
+    
+    var body: some View {
+        NavigationStack {
+            SortedToDoList(sortSelection: sortSelection)
+            .navigationTitle("To Do List")
+            .navigationBarTitleDisplayMode(.automatic)
+            .sheet(isPresented: $sheetIsPresented) {
+                NavigationStack {
+                    DetailView(toDo: ToDo())
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        sheetIsPresented.toggle()
+                    } label: {
+                        Image(systemName: "plus")
+                    }
+                }
+                ToolbarItem(placement: .bottomBar) {
+                    Picker("", selection: $sortSelection) {
+                        ForEach(SortOption.allCases, id: \.self) { sortOrder in
+                            Text(sortOrder.rawValue)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ToDoListView()
+        .modelContainer(ToDo.preview)
+}
